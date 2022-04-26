298759d479c9769c060abd88398294e26faba5b7
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-04-27 00:37:15 +0100

    chijiuba

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int index = 0;
+	int index;
 
-	while (s[index] != '\0')
+	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[index] == c)
 			return (s + index);
-		index++;
 	}
 
 	return ('\0');
 }
-
