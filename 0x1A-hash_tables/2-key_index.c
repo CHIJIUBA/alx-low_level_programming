a7a5a5de5d43d4af20a59e2cc2b34491ed43af15
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-28 16:45:03 -0700

    go

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,16 +6,16 @@
  * @size: the size of the array of the hash table
  * @key: the key
  *
- * Return: the index at which the key/value pair 
+ * Return: the index at which the key/value pair
  * should be stored in the array of the hash table;
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
-    unsigned long int hash_value;
+	unsigned long int index;
+	unsigned long int hash_value;
 
-    hash_value = hash_djb2(key);
-    index = hash_value % size;
+	hash_value = hash_djb2(key);
+	index = hash_value % size;
 
-    return (index);
+	return (index);
 }
