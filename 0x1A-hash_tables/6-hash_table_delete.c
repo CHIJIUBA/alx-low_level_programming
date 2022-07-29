91d074e04da9fe145b129e17c60030fa77ecdae6
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-29 23:29:09 +0100

    go

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,20 +6,15 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-<<<<<<< HEAD
 	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
-=======
-	hash_node_t *item, *temp;
-	hash_table_t *head = ht;
->>>>>>> 634783f77201e241a080c2660f43153ab940e8cd
+
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-<<<<<<< HEAD
 			node = ht->array[i];
 			while (node != NULL)
 			{
@@ -28,17 +23,6 @@ void hash_table_delete(hash_table_t *ht)
 				free(node->value);
 				free(node);
 				node = tmp;
-=======
-			item = ht->array[i];
-
-			while (item != NULL)
-			{
-				temp = item->next;
-				free(item->key);
-				free(item->value);
-				free(item);
-				item = temp;
->>>>>>> 634783f77201e241a080c2660f43153ab940e8cd
 			}
 		}
 	}
