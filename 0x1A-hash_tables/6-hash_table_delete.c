085dac23300e3208ad0a21899d5bffb8a504616b
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-29 23:24:36 +0100

    go

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,36 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete -  deletes a hash table
- *
- * @ht: The hash table to delete
- *
- * Return: NULL;
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *item, *temp;
-    hash_table_t *head = ht;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-            item = ht->array[i];
-
-			while (item != NULL)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-                temp = item->next;
-				free(item->key);
-				free(item->value);
-                free(item);
-				item = temp;
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
 		}
 	}
-
 	free(head->array);
 	free(head);
 }
-
