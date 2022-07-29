abb2ceb8e5cb2a9349f08a8c922c24561c022639
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-29 03:58:01 +0100

    go

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get -  retrieves a value associated 
+ * with a key from hash table
+ *
+ * @ht: hash table you want to look into
+ * @key: the key you are looking for
+ *
+ * Return: The value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *item;
+    unsigned long int index;
+
+    index = key_index((const unsigned char *)key, ht->size);
+    item = ht->array[index];
+
+    while (item != NULL)
+    {
+        if (strcmp(item->key, key) == 0)
+            return (item->value);
+        item = item->next;
+    }
+
+    return (NULL);
+}
